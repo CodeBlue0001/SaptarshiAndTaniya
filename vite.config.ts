8e241f7e0855f0d9e44d6adeea07c0652cff4113
CodeBlue0001 <145474893+CodeBlue0001@users.noreply.github.com>
2025-06-03 18:51:32 +0530

    Update vite.config.ts changeing port into link

--- vite.config.ts
+++ vite.config.ts
@@ -5,27 +5,27 @@ import path from "path";
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
   server: {
-
-    strictPort: false,
-    allowedHosts: ["saptarshiandtaniya.onrender.com"], // 👈 Add this line
+    host: "0.0.0.0",           // Allow access from LAN/external devices
+    port: 'https://saptarshiandtaniya.onrender.com/',                // Default port
+    strictPort: false,         // Use next port if 8080 is busy
   },
   preview: {
-    host: "0.0.0.0",
+    host: "0.0.0.0",           // Preview server also accessible externally
     port: 4173,
     strictPort: false,
   },
   plugins: [react()],
   resolve: {
     alias: {
-      "@": path.resolve(__dirname, "./src"),
+      "@": path.resolve(__dirname, "./src"), // Shortcut for imports like @/components/...
     },
   },
   build: {
     outDir: "dist",
     assetsDir: "assets",
     sourcemap: false,
-    minify: "esbuild",
-    target: "es2015",
+    minify: "esbuild",         // Fast build with esbuild
+    target: "es2015",          // Broad browser support
     rollupOptions: {
       output: {
         manualChunks: {
