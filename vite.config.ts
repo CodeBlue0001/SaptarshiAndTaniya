956371235cd8edf8d0c1f96c862a92fabb353981
CodeBlue0001 <145474893+CodeBlue0001@users.noreply.github.com>
2025-06-03 18:40:43 +0530

    Update vite.config.ts
    tying to not set fixed port

--- vite.config.ts
+++ vite.config.ts
@@ -5,8 +5,7 @@ import path from "path";
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
   server: {
-    host: "0.0.0.0",
-    port: 8080,
+
     strictPort: false,
     allowedHosts: ["saptarshiandtaniya.onrender.com"], // 👈 Add this line
   },
